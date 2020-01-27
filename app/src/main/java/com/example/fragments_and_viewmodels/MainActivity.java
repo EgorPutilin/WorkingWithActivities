55d32ade019c977606aff088015c2780665b7442
EgorPutilin <59206800+EgorPutilin@users.noreply.github.com>
2020-01-27 18:05:39 +0300

    _commit

--- app/src/main/java/com/example/fragments_and_viewmodels/MainActivity.java
+++ app/src/main/java/com/example/fragments_and_viewmodels/MainActivity.java
@@ -4,13 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txtText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtText = findViewById(R.id.txtText);
+        txtText.setText("Some text");
         Log.d("Lifecycle", "onCreate");
     }
 
